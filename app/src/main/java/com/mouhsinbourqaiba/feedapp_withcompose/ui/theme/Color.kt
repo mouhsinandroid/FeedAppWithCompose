57f71d801d23fb41fa7e6461ba4c2849ce1cd550
m.bour-qaiba <mouhsin.bourqaiba-ext@montreal.ca>
2024-02-25 14:11:13 -0500

    feat - add spashscreen api

--- app/src/main/java/com/mouhsinbourqaiba/feedapp_withcompose/ui/theme/Color.kt
+++ app/src/main/java/com/mouhsinbourqaiba/feedapp_withcompose/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.mouhsinbourqaiba.feedapp_withcompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+object AppColors {
+    val primary = Color(0xFF1E88E5) // Example: Blue for headers or important buttons
+    val primaryVariant = Color(0xFF005CB2) // Darker shade for the status bar, etc.
+    val secondary = Color(0xFFE53935) // Example: Red for accents
+    val background = Color(0xFFFFFFFF) // White background
+    val surface = Color(0xFFFFFFFF) // For cards, sheets, and menus
+    val onPrimary = Color(0xFFFFFFFF) // Text color on primary color
+    val onSecondary = Color(0xFFFFFFFF) // Text color on secondary color
+    val onBackground = Color(0xFF000000) // Typically black or dark grey for readability
+    val onSurface = Color(0xFF000000) // Text color on surface color
+}
